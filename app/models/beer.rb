6b7f95dcdcacb9a469cf7340a4dd623d4bf16f5d
evgeny nazarenko <nazarenkoe@me.com>
2019-08-20 10:15:19 +0200

    Has_many add

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,7 @@ class Beer < ApplicationRecord
   belongs_to :category
   belongs_to :color
   belongs_to :brewery
+  has_many :beer_foods
   validates :name, presence: true, uniqueness: true
   validates :photo, presence: true
   validates :alcohol, presence: true
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ApplicationRecord
+  has_many :breweries, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
