82b84225f80c21be45dc87f8667b204dea0833c9
TATIANE GRASSI <tatianegrassi@gmail.com>
2019-08-22 16:11:30 +0200

    home page design done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get 'beers/search', to: 'beers#search', as: :beer_search
   root to: 'pages#home'
+  get 'barcode-scanner', to: 'pages#barcode'
   get 'design', to: 'pages#design', as: :design
   get 'loading', to: 'pages#loading', as: :loading
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
