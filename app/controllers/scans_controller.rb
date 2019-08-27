c05accf78e8e385bbcdab30cdf8cdabff53af3e8
Amanda Schjørmann <amandaschjoermann@gmail.com>
2019-08-27 10:13:43 +0200

    add beerscan to homepage

---
+++ app/controllers/scans_controller.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+require 'open-uri'
+
+class ScansController < ApplicationController
+
+  def create
+    @scan = Scan.new(scan_params)
+    if @scan.save
+      split_url = @scan.photo.metadata["url"].split("upload/")
+      formatted_url = split_url[0] + "upload/w_400,c_scale/" + split_url[1]
+      url = "http://zxing.org/w/decode?full=true&u=#{formatted_url}"
+      html_file = open(url).read
+      html_doc = Nokogiri::HTML(html_file)
+      barcode = html_doc.search('pre').first.inner_text
+      @beer = Beer.find_by(upc: barcode)
+      redirect_to beer_path(@beer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def scan_params
+    params.require(:scan).permit(:photo)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_122925) do
+ActiveRecord::Schema.define(version: 2019_08_26_174022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,12 @@ ActiveRecord::Schema.define(version: 2019_08_22_122925) do
   create_table "glasses", force: :cascade do |t|
     t.string "photo"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scans", force: :cascade do |t|
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
