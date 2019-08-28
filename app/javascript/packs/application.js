ba71a315741812280390e685fd284c6c4c6b1f04
Martino Fabbro <martino.fabbro@gmail.com>
2019-08-28 18:34:58 +0200

    iphone + landing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,7 +23,18 @@ $(".search-icon").click (function() {
 $(".spinner").hide();
 
 
-$(window).bind("beforeunload", function(){
+$(window).bind("onbeforeunload", function(){
   $("#barcode-picture-home").hide();
   $(".spinner").fadeIn('slow');
 });
+
+// document.querySelector(".spinner").style.display = "none";
+
+// window.unload() => {
+//   document.querySelector("#barcode-picture-home").style.display = "block";
+//   document.querySelector(".spinner")fadeIn('slow');
+// }
+
+// window.addEventListener('unload', function(event) {
+//         document.querySelector("#barcode-picture-home").style.display = "block";
+//       });
